c4aa8cb169a2f03fe1f037eb96fcc996d8357b93
legend507 <legend507@legend507MAC.local>
2018-01-28 23:01:08 +0900

    Trying 218, goldman interview question

---
+++ LeetCodeNo.218/main.cpp
@@ -0,0 +1,18 @@
+#include <vectpr>
+#include <pair>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<pair<int, int>> getSkyline(vector<vector<int>>& buildings) {
+        
+    }
+};
+
+int main() {
+    Solution s;
+
+    return 0;
+}
+
