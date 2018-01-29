9db497de4ac3fd82ee0d6e8fd9718f3b87eedf91
legend507 <legend507@legend507MAC.local>
2018-01-29 22:26:06 +0900

    Trying to solve No.218, I think I can solve it tomorrow.

--- LeetCodeNo.218-doing/main.cpp
+++ LeetCodeNo.218-doing/main.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <iostream>
 #include <climits>
+#include <set>
 using namespace std;
 
 /* Initial thinking */
@@ -12,8 +13,23 @@ using namespace std;
 
 class Solution {
 public:
-    vector<pair<int, int>> getSkyline(vector<vector<int>>& buildings) {
-        vector<pair<int, int>> ret;
+    // input: [x_left, x_right, height]
+    vector<pair<int, int> > getSkyline(vector<vector<int> >& buildings) {
+        vector<pair<int, int> > ret;
+
+        // multiset automatically sorts all input (ascending by default)
+        multiset<pair<int, int> > criticalPoints;
+
+        // 1. traverse buildings, find the "critical points", namely [x_left, height] and [x_right, height]
+        for(auto oneBuilding:buildings) {
+            // [x_left, -height], why -height? because we know the point is the left point of a building
+            criticalPoints.emplace(make_pair(oneBuilding[0], -oneBuilding[2]));
+            // [x_right, height]
+            criticalPoints.emplace(make_pair(oneBuilding[1],  oneBuilding[2]));            
+        }
+
+        // 2. traverse all "critical points" to draw skyline
+        multiset<int> highest({0});
 
         return ret;
     }
@@ -21,7 +37,8 @@ public:
 
 int main() {
     Solution s;
-    vector<vector<int>> buildings = { {2,9,10}, {3,7,15}, {5,12,12}, {15,20,10}, {19,24,8} };
+
+    vector<vector<int> > buildings = { {2,9,10}, {3,7,15}, {5,12,12}, {15,20,10}, {19,24,8} };
 
     s.getSkyline(buildings);
 
