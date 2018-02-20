95bf1a90dd4e93ea06c1637dfb3453a48842c170
legend507 <chenfeiyu507@gmail.com>
2018-02-20 19:00:56 +0900

    Solving 188

--- 0GeneralDebug/0GeneralDebug/main.cpp
+++ 0GeneralDebug/0GeneralDebug/main.cpp
@@ -1,135 +1,57 @@
-#include    <iostream>
+﻿#include    <iostream>
 #include    <queue>
 #include	<functional>
 #include	<stack>
 #include	<string>
 #include	<iostream>
 #include	<unordered_set>
+#include	<sstream>			// istringstream and ostringstream
+#include	<set>
 using namespace std;
 
-/*
-To build a priority queue,
-with a pointer to median
-*/
-class Solution {
-public:
-	vector<string> removeInvalidParentheses(string s) {
-		unordered_set<string> ret1;
 
-		string oneRet;
-		stack<char> leftParenthese;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
 
-		int leftP2Remove = 0, rightP2Remove = 0;
-		for (auto oneChar : s) {
-			// assume no ) will be found later, remove this (
-			if (oneChar == '(') {
-				leftP2Remove++;
-			}
-			// found a ), pair this ) with former (; if no former (, remove this )
-			else if (oneChar == ')') {
-				if (leftP2Remove != 0)
-					leftP2Remove--;
-				else
-					rightP2Remove++;
-			}
-		}
-
-		recurseRemove(ret1, oneRet, s, leftParenthese, leftP2Remove, rightP2Remove);
-
-		//	2nd, check all possible rets, 
-		vector<string> ret(ret1.begin(), ret1.end());
-		for (auto oneStr : ret) {
-			cout << oneStr << endl;
-		}
-
-		for (vector<string>::iterator itr = ret.begin(); itr != ret.end(); /*no itr++*/) {
-			stack<char> leftParenthesesTracker;
-
-			for (auto oneChar : (*itr)) {
-				if (oneChar == '(')
-					leftParenthesesTracker.push('(');
-				if (oneChar == ')')
-					leftParenthesesTracker.pop();
-			}
-			if (!leftParenthesesTracker.empty())
-				itr = ret.erase(itr);
-			else
-				itr++;
-		}
-
-		if (!ret.empty())
-			return ret;
-		else {
-			vector<string> empty = { "" };
-			return empty;
-		}
-	}
-
-	void recurseRemove(unordered_set<string> & ret, 
-		string oneRet, string s, 
-		stack<char> leftParenthese, 
-		int leftP2Remove, 
-		int rightP2Remove) {
-
-		// traverse the input string
-		for (int i = 0; i < s.size(); i++) {
-
-			// if not ( and not ), append
-			if (s[i] != '(' && s[i] != ')') {
-				oneRet += s[i];
-			}
-			// if (
-			else if (s[i] == '(') {
-				// 1. not put current ( into one ret
-				if(leftP2Remove > 0)
-					recurseRemove(ret, oneRet, s.substr(i + 1, s.size() - i), leftParenthese, leftP2Remove-1, rightP2Remove);
-
-				// 2. put current ( into one ret
-				leftParenthese.push(s[i]);
-				oneRet += s[i];
-			}
-			// if ) and there is former (, 
-			else if (s[i] == ')') {
-				if (leftParenthese.empty()) {
-					rightP2Remove--;
-					continue;
-				}
-
-				// 1. not put current ) in final answer
-				if(rightP2Remove > 0)
-					recurseRemove(ret, oneRet, s.substr(i + 1, s.size() - i), leftParenthese, leftP2Remove, rightP2Remove-1);
-
-				// 2. put current ) in final answer
-				leftParenthese.pop();
-				oneRet += s[i];
-			}
-		}
-
-		if(leftP2Remove ==0 && rightP2Remove==0)
-			ret.insert(oneRet);
-	}
-
+	// Constructor
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-/**
-* Your MedianFinder object will be instantiated and called as such:
-* MedianFinder obj = new MedianFinder();
-* obj.addNum(num);
-* double param_2 = obj.findMedian();
-*/
+class Solution {
+public:
+	int maxProfit(int k, vector<int>& prices) {
+		int size = prices.size();
+		queue<int>	peakIndex;
+		queue<int> valleyIndex;
+
+		// try to find all adjacent peak, valley
+		//	put 0~size-1 to peakIndex or valleyIndex
+		prices[0] > prices[1] ? peakIndex.push(0) : valleyIndex.push(0);
+		for (int i = 1; i < prices.size() - 1; i++) {
+			// i-1 < i > i+1
+			if (prices[i-1] < prices[i]	
+				&& prices[i] > prices[i+1]) {
+
+				peakIndex.push(i);
+			}
+			// i-1 > i < i+1
+			else if (prices[i-1] > prices[i]
+				&& prices[i] < prices[i+1]){
+
+				valleyIndex.push(i);
+			}
+		}
+		prices[size - 2] > prices[size - 1] ? valleyIndex.push(size - 1) : peakIndex.push(size - 1);
+
+
+
+	}
+};
+
 
 int main() {
-	Solution sol;
-
-	string input = { ")(f" };
-
-	vector<string> ret;
-
-	ret = sol.removeInvalidParentheses(input);
-
-	for (auto oneStr : ret) {
-		cout << oneStr << endl;
-	}
 
 	system("pause");
 	return 0;
