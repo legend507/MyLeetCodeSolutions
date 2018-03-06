be7d8bb2fee89c73799095b91a9a7a911354b7da
legend507 <legend507@legend507MAC.local>
2018-03-06 22:18:17 +0900

    Solve the 1??0?101 problem in my way, need to check if there is a better solution

--- 0GeneralDebug/0GeneralDebug/main.cpp
+++ 0GeneralDebug/0GeneralDebug/main.cpp
@@ -1,16 +1,16 @@
 ﻿/*
-Given a string containing of ‘0’, ‘1’ and ‘?’ wildcard characters, generate all binary strings that can be formed by replacing each wildcard character by ‘0’ or ‘1’.
-
-Input str = "1??0?101"
-Output:
-			10000101
-			10001101
-			10100101
-			10101101
-			11000101
-			11001101
-			11100101
-			11101101
+ Given a string containing of ‘0’, ‘1’ and ‘?’ wildcard characters, generate all binary strings that can be formed by replacing each wildcard character by ‘0’ or ‘1’.
+ 
+ Input str = "1??0?101"
+ Output:
+                 10000101
+                 10001101
+                 10100101
+                 10101101
+                 11000101
+                 11001101
+                 11100101
+                 11101101
  */
 
 #include <vector>
@@ -32,53 +32,62 @@ using namespace std;
 //struct TreeNode
 //{
 //    T data;
-//	TreeNode* left, *right;
-//    
-//	TreeNode(T x) : data(x), left(NULL), right(NULL) {}
+//    TreeNode* left, *right;
+//
+//    TreeNode(T x) : data(x), left(NULL), right(NULL) {}
 //};
 
 struct TreeNode
 {
-	char data;
-	TreeNode* left, *right;
-
-	TreeNode(char x) : data(x), left(NULL), right(NULL) {}
+    char data;
+    TreeNode* left, *right;
+    
+    TreeNode(char x) : data(x), left(NULL), right(NULL) {}
 };
 
 class StringGenerator {
-	
-
+    
+    
 public:
-	vector<string> recurseMethod(string str) {
-		vector<int> index;
-		for (int i = 0; i < str.size(); i++)
-			if (str[i] == '?')
-				index.push_back(i);
-
-		vector<string> ret;
-		recurseMethod(ret, index, str);
-
-		return ret;
-	}
-	void recurseMethod(vector<string>& ret, vector<int>& index, string str) {
-
-	}
+    /*
+     The following is my recurse method
+     */
+    vector<string> recurseMethod(string str) {
+        vector<int> index;
+        for (int i = 0; i < str.size(); i++)
+            if (str[i] == '?')
+                index.push_back(i);
+        
+        vector<string> ret;
+        recurseMethod(ret, index, str, 0);
+        
+        return ret;
+    }
+    void recurseMethod(vector<string>& ret, vector<int>& index, string str, int pos) {
+        // base case
+        if(pos == index.size()) {
+            ret.push_back(str);
+            return;
+        }
+        
+        // recurse case
+        str[index[pos]] = '0';
+        recurseMethod(ret, index, str, pos+1);
+        str[index[pos]] = '1';
+        recurseMethod(ret, index, str, pos+1);
+    }
 };
 
 
 int main() {
-	TreeNode *root = new TreeNode('A');
-	root->left = new TreeNode('B');
-	root->right = new TreeNode('C');
-	root->left->left = new TreeNode('D');
-	root->left->right = new TreeNode('E');
-	root->right->right = new TreeNode('B');
-	root->right->right->right = new TreeNode('E');
-	root->right->right->left = new TreeNode('D');
-
-	root->right->right->right->left = new TreeNode('S');
-
-
-	system("pause");
+    StringGenerator sg;
+    vector<string> ret = sg.recurseMethod("1??0?101");
+    
+    for(auto oneStr : ret) {
+        cout << oneStr << endl;
+    }
+    
+    system("pause");
     return 0;
 }
+
