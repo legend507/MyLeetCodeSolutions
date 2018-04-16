7fda261e0f6c341ed60c5f839d1b19fae1d214e1
legend507 <chenfeiyu507@gmail.com>
2018-04-16 17:48:28 +0900

    solved 54, 56

---
+++ LeetCodeNo.54/main.cpp
@@ -0,0 +1,116 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <queue>
+#include <string>
+#include <stack>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> spiralOrder(vector<vector<int>>& matrix) {
+		vector<int> ret;
+		if (matrix.size() == 0) return ret;
+
+
+		int row = matrix.size();
+		int col = matrix[0].size();
+		int STEP = row * col;
+		int count = 0;
+		vector<vector<bool>> visited(row, vector<bool>(col, false));
+
+		int i = 0;	// for row
+		int j = 0;	// for col
+
+		int dir = 0x0;	// 0x0, 0x1, 0x2, 0x3, 4 directions
+
+		while (count < STEP) {
+			visited[i][j] = true;	// mark current(i,j) visited
+			ret.push_back(matrix[i][j]);
+			count++;
+
+			// check next step, based on current dir
+			switch (dir) {
+			case 0x0:
+				// current dir right, j++
+				//	if next step is out of boundry or next step visited
+				if (j + 1 == col || visited[i][j + 1] == true) {
+					dir = 0x1;	// change dir to down
+					i = i + 1;	// 
+					j = j;		//
+				}
+				else {
+					i = i;
+					j = j + 1;	// go one step right
+				}
+				break;
+			case 0x1:
+				// current dir down, i++
+				//	if next step is out of boundry or next step visited
+				if (i + 1 == row || visited[i + 1][j] == true) {
+					dir = 0x2;	// change dir to left
+					i = i;	// 
+					j = j - 1;		//
+				}
+				else {
+					i = i + 1;	// go one step down
+					j = j;
+				}
+				break;
+			case 0x2:
+				// current dir left, j--
+				//	if next step is out of boundry or next step visited
+				if (j - 1 == -1 || visited[i][j - 1] == true) {
+					dir = 0x3;	// change dir to up
+					i = i - 1;
+					j = j;
+				}
+				else {
+					i = i;	// go one step down
+					j = j - 1;
+				}
+				break;
+			case 0x3:
+				// current dir up, i--
+				//	if next step is out of boundry or next step visited
+				if (i - 1 == -1 || visited[i - 1][j] == true) {
+					dir = 0x0;	// change dir to right
+					i = i;
+					j = j + 1;
+				}
+				else {
+					i = i - 1;	// go one step down
+					j = j;
+				}
+				break;
+			}
+
+		}
+		return ret;
+	}
+};
+
+int main()
+{
+	vector<vector<int>> matrix =
+	{
+		{ 1, 2, 3 },
+	{ 4, 5, 6 },
+	{ 7, 8, 9 },
+	{ 11, 12, 100 }
+	};
+
+
+	Solution s;
+
+	vector<int> ret = s.spiralOrder(matrix);
+
+
+
+	system("pause");
+	return 0;
+}
+
---
+++ LeetCodeNo.56/main.cpp
@@ -0,0 +1,68 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <queue>
+#include <string>
+#include <stack>
+#include <unordered_set>
+using namespace std;
+
+
+struct Interval {
+	int start;
+	int end;
+	Interval() : start(0), end(0) {}
+	Interval(int s, int e) : start(s), end(e) {}
+
+	static bool compare(Interval& i1, Interval& i2) {
+		return (i1.start < i2.start);
+	}
+};
+
+class Solution {
+public:
+	vector<Interval> merge(vector<Interval>& intervals) {
+		if (intervals.size() <2) return intervals;
+
+
+		sort(intervals.begin(), intervals.end(), Interval::compare);
+
+		for (int i = 0; i < intervals.size() - 1; i++) {
+			Interval i1 = intervals[i];
+			Interval i2 = intervals[i + 1];
+
+			// overlaped
+			if (i1.end >= i2.start) {
+				// erase 
+				intervals.erase(intervals.begin() + i, intervals.begin() + i + 1 + 1);
+
+				// merge into a new Interval
+				Interval merged(i1.start, max(i1.end, i2.end));
+				intervals.insert(intervals.begin() + i, merged);
+				i--;
+			}
+
+		}
+
+		return intervals;
+	}
+};
+
+int main()
+{
+
+
+	vector<Interval> nums = { { 1,4 },{ 2,3 } };
+
+	Solution s;
+
+	s.merge(nums);
+
+
+
+	system("pause");
+	return 0;
+}
+
